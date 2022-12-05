207b4c3ff33cb251bc2ecd5a9084b77a80ee1625
Salamat Kassenova <kassenova1993@gmail.com>
2022-12-05 07:08:21 +0600

    1

---
+++ Zadacha2/Zadacha2.cs
@@ -0,0 +1,13 @@
+﻿//2
+Console.WriteLine("Введите целое число a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое число b");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+    Console.WriteLine($"max = {a}");
+}
+else 
+{
+    Console.WriteLine($"max = {b}");
+}
