8f7af8ee8b67acb21c97d517a35edc23bfe4fb49
Salamat Kassenova <kassenova1993@gmail.com>
2022-12-15 11:08:53 +0600

    6

---
+++ HomeWork6/Zadacha41/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите целые числа через пробел");
+int[] str = Console.ReadLine().Split().Select(int.Parse).ToArray();
+Positive(str);
+
+void Positive(int[] str)
+{   
+    int count = 0;
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (str[i]>0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine(count);
+}
---
+++ HomeWork6/ZadachaHARD_1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите целое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int Fibonacci(int n)
+{
+    if (n==0) return 0;
+    if(n==1||n==2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+for (int i = 0; i <+ n; i++)
+{
+    
+}
